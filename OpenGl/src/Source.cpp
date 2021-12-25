50a39a0c8d5137b6958972d7f4bf2fc7d59b70e4
jumail <jumailjuraige@gmail.com>
2021-12-25 15:29:12 +0530

    opnegl utility change from glew to glad

--- OpenGl/src/Source.cpp
+++ OpenGl/src/Source.cpp
@@ -1,11 +1,39 @@
-#include <iostream>
-#include <GL/glew.h>
+#include<iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-void main(void) {
+int main() {
+	std::cout << glfwInit() << std::endl;
 
-	std::cout<< glfwInit() << std::endl;
-	std::cout << glewInit() << std::endl;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	system("PAUSE");
+	GLFWwindow* window = glfwCreateWindow(800, 600, "window", NULL, NULL);
+	if (!window) 
+	{ 
+		glfwTerminate(); 
+		return 0; 
+	}
+
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+	glViewport(0, 0, 800, 600);
+
+	glClearColor(0.134f, 0.132f, 0.703f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+
+
 }
